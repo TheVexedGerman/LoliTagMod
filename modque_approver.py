0d955507b0b1aa0670d118befdc80b027d16f1f2
TheVexedGerman <jmohr11@gmail.com>
2020-08-06 14:23:33 +0200

    Fix lower function

--- modque_approver.py
+++ modque_approver.py
@@ -106,7 +106,7 @@ def run_bot():
             if "I didn't find any posts that meet the matching requirements" in comment.body:
                 comment.mod.approve()
                 continue
-        if comment.body.lower == 'trap' and comment.banned_by == 'AutoModerator':
+        if comment.body.lower() == 'trap' and comment.banned_by == 'AutoModerator':
             comment.mod.remove()
             continue
         has_numbers, has_redaction = check_for_violation(comment.body)
